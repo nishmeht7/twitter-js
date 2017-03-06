29af97d32e46d0cc3f6d878d8ebd6de256c951d0
Nishant Mehta <nishmeht7@gmail.com>
2017-03-06 17:25:21 -0600

    config done

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express'); 
+const volleyball = require('volleyball');
+
+var app = express(); 
+
+app.use(volleyball);
+
+// app.use(function(req, res, next){
+// 	console.log(req.method+" "+req.url+" "+res.statusCode); 
+	
+// 	next();
+// })
+
+app.get('/', function(req, res){
+	//console.log(res)
+	res.send("Welcome to Twitter Bitch!");
+
+});
+
+app.get('/news', function(req, res){
+
+	res.send("Twitter sucks!");
+});
+
+app.listen(3000, function(){
+	console.log("Using port number 3000"); 
+});
