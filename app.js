ebac891942ec62e37276ab473d25e4f0dd145862
Nishant Mehta <nishmeht7@gmail.com>
2017-03-07 10:22:19 -0600

    Nish's Driving over

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 const express = require('express'); 
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 var app = express(); 
 
-nunjucks.configure('/views/index.html');
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true}); // point nunjucks to the proper directory for templates
 
-app.use(volleyball);
-
-// app.use(function(req, res, next){
-// 	console.log(req.method+" "+req.url+" "+res.statusCode); 
-	
-// 	next();
+// nunjucks.render('index.html', {title: 'An Example', people: [{name: 'gandolf'}, {name: 'hermoione'}, {name: 'Frodo'}]}, function(err, res){
+// 	if (err) throw err; 
+// 	console.log(res); 
 // })
 
-app.get('/', function(req, res){
-	//console.log(res)
-	res.send("Welcome to Twitter Bitch!");
+app.use(volleyball); //volleyball is middleware that gets the status of the response 
 
+app.get('/', function(req, res){
+	res.render( 'index', {title: 'Welcome to twitter Bitch', people: people} );
 });
 
 app.get('/news', function(req, res){
-
-	res.send("Twitter sucks!");
+	res.render('index', {title:"Twitter sucks!"});
 });
 
 app.listen(3000, function(){
---
+++ tweetBank.js
@@ -0,0 +1,52 @@
+const _ = require('lodash');
+
+var data = []; //stores all the tweets 
+
+function add (name, content) {
+  data.push({ name: name, content: content });
+}
+
+function list () {
+  return _.cloneDeep(data);
+}
+
+function find (properties) {
+  return _.cloneDeep(_.filter(data, properties));
+}
+
+module.exports = { 
+	add: add, 
+	list: list, 
+	find: find 
+};
+
+
+
+//Fake Tweets
+
+const randArrayEl = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const getFakeName = function() {
+  const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
+  const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+};
+
+const getFakeTweet = function() {
+  const awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing', 'impressive'];
+  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+};
+
+for (let i = 0; i < 10; i++) {
+  module.exports.add( getFakeName(), getFakeTweet() );
+}
+add('Donald Trump', 'Build a Wall'); 
+var asshole = find({name: 'Donald Trump'}); 
+asshole.forEach(function(tweets){
+	console.log(tweets.content);
+})
+
+
+console.log(data); 
